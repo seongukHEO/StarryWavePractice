7b8ba0bbfec7f10adec5f8c57a38c2976e801236
seongukHEO <heoseonguk3@gmail.com>
2024-08-10 09:47:36 +0900

    feat : 당장 해야할 항목 리스트 구현

---
+++ lib/common/now_going_list.dart
@@ -0,0 +1,64 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NowGoingList extends StatefulWidget {
+  const NowGoingList({super.key});
+
+  @override
+  State<NowGoingList> createState() => _NowGoingListState();
+}
+
+class _NowGoingListState extends State<NowGoingList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      child: ListView.builder(
+          itemCount: 3,
+          itemBuilder: (context, index){
+            return Container(
+              margin: EdgeInsets.fromLTRB(0, 8, 0, 0),
+              decoration: BoxDecoration(
+                  color: Colors.grey.shade300,
+                  borderRadius: BorderRadius.circular(8)
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("게임인문학 과제 제출", style: TextStyle(fontWeight: FontWeight.bold),),
+                        Container(
+                          margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                          width: 35,
+                          height: 20,
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade400,
+                            borderRadius: BorderRadius.circular(10)
+                          ),
+                          child: Align(
+                            alignment: Alignment.center,
+                              child: Text("집중", style: TextStyle(fontSize: 10, color: Colors.grey.shade700),)),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: IconButton(
+                      onPressed: (){},
+                      icon: Icon(Icons.play_circle_filled_rounded),
+                    ),
+                  )
+                ],
+              ),
+            );
+          }
+      ),
+    );
+  }
+}
--- lib/widget/today_todo_widget.dart
+++ lib/widget/today_todo_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:starrywave_practice/common/now_going_list.dart';
 
 class TodayTodoWidget extends StatefulWidget {
   const TodayTodoWidget({super.key});
@@ -10,6 +12,22 @@ class TodayTodoWidget extends StatefulWidget {
 class _TodayTodoWidgetState extends State<TodayTodoWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(18),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("2024년 7월", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+              SizedBox(height: 100,),
+              Text("지금! 당장 바로 해야하는\n3가지를 아래에서 골라 보세요!", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
+              SizedBox(height: 10,),
+              NowGoingList(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
