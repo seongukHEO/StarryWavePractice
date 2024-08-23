3eaefbbcba2bd4f63cc0e6ed1f1a6c7c242f2901
seongukHEO <heoseonguk3@gmail.com>
2024-08-24 08:00:55 +0900

    feat : task repository 추가

---
+++ lib/domain/repo/task_repository.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:starrywave_practice/data/firebase_task_dataSource.dart';
+import 'package:starrywave_practice/model/taskModel.dart';
+
+class TaskRepository {
+  final FirebaseTaskDatasource firebaseTaskDatasource;
+
+  TaskRepository(this.firebaseTaskDatasource);
+
+
+  //데이터 추가하기
+  Future<void> addTask(TaskModel task)async{
+    final taskModel = TaskModel(
+      jobId: task.jobId,
+      fomodoroIdx: task.fomodoroIdx,
+      totalTime: task.totalTime,
+    );
+    return firebaseTaskDatasource.addTask(taskModel);
+  }
+
+  //데이터 가져오기
+  Stream<List<TaskModel>>getTask(String jobId){
+    return firebaseTaskDatasource.getTask(jobId);
+  }
+}
